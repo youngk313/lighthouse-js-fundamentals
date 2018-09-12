999b9fac13a08b1cab5e8dac1cc4f40c7137b556
Young <youngkwon313@gmail.com>
2018-09-12 18:21:41 +0000

    Ingredients exer has been added

---
+++ ingredients.js
@@ -0,0 +1,14 @@
+var ingredients = ['eggs','milk','flour','sugar','baking soda','baking powder','chocolate chips','bananas'];
+var i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+
+for (i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
+for (i = ingredients.length - 1; i > -1; i--) {
+  console.log(ingredients[i]);
+}
